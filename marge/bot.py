faa290d1d33ddad98ada5bc90e41025b81546036
Matthew Pickering <matthewtpickering@gmail.com>
2019-02-09 00:25:47 +0000

    Remove logic which tries one job on batch failure

--- marge/bot.py
+++ marge/bot.py
@@ -153,13 +153,6 @@ class Bot(object):
                 return
             except git.GitError as err:
                 log.exception('BatchMergeJob failed: %s', err)
-        log.info('Attempting to merge the oldest MR...')
-        merge_request = merge_requests[0]
-        merge_job = self._get_single_job(
-            project=project, merge_request=merge_request, repo=repo,
-            options=self._config.merge_opts,
-        )
-        merge_job.execute()
 
     def _get_single_job(self, project, merge_request, repo, options):
         return single_merge_job.SingleMergeJob(
